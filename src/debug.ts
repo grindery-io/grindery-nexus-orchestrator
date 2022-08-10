b98fb8f1468965bedf199bf0d251408563e64bd9
SAPikachu <sapikachu@gmail.com>
2022-08-10 18:07:43 +0800

    Small change

--- src/debug.ts
+++ src/debug.ts
@@ -4,10 +4,9 @@ export function createDebug(name: string) {
   if (!process.env.LOG_JSON) {
     return debug(name);
   }
-  const isEnabled = debug.enabled(name);
   return (...args) => {
-    if (isEnabled) {
-      console.debug(...args, { source: name });
+    if (debug.enabled(name)) {
+      console.log(...args, { source: name });
     }
   };
 }
