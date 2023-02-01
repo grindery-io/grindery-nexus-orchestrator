c3b49f14095ccf01529634d5c927fd1a6ae2426e
SAPikachu <i@sapika.ch>
2023-02-01 01:36:35 +0000

    Enable sentry

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
+import * as Sentry from "@sentry/node";
 import { LoggerAdaptToConsole, LOG_LEVEL } from "console-log-json";
 import routes from "./routing";
 import { runJsonRpcServer } from "grindery-nexus-common-utils/dist/server";
 import { createServer } from "./jsonrpc";
 
-
+if (process.env.SENTRY_DSN) {
+  Sentry.init();
+}
 if (process.env.LOG_JSON) {
   LoggerAdaptToConsole({ logLevel: LOG_LEVEL.debug });
 }
