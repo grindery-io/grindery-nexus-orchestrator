8bb8d8a0aa33c0afc3150dd1fb4688b8d0ec35bd
SAPikachu <sapikachu@gmail.com>
2023-07-31 18:07:41 +0800

    Update auth frontend URL

--- src/routing/oauth.ts
+++ src/routing/oauth.ts
@@ -156,7 +156,7 @@ router.get("/authorize", async (req, res) => {
       return res.status(400).json({ error: "invalid_request" });
     }
   }
-  const url = new URL(`https://${process.env.AUTH_FRONTEND_DOMAIN || "flow.grindery.org"}/sign-in`);
+  const url = new URL(process.env.AUTH_FRONTEND_URL || "https://login.grindery.io/");
   for (const key of Object.keys(req.query)) {
     url.searchParams.set(key, String(req.query[key]));
   }
